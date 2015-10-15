9c13297105556e71b744b932843f4046406ee478
bakasajoshua <baksajoshua09@gmail.com>
2015-10-15 14:09:02 +0300

    start of the loan approval in the manager module. Still with some major bugs.

--- application/modules/manager/controllers/manager.php
+++ application/modules/manager/controllers/manager.php
@@ -9,6 +9,7 @@ class Manager extends MY_Controller
 	{
 		parent::__construct();
 		$this->login_reroute(2);
+		$this->load->module('loans');
 	}
 
 	public function index()
@@ -21,6 +22,54 @@ class Manager extends MY_Controller
 	$data['module'] = "dashboard";
 	$data['view_file'] = "home";
 	echo Modules::run('template/manager', $data);
+	}
+
+	function loans()
+	{
+		$data['section'] = "ADI Sacco";
+	    $data['subtitle'] = "Manager";
+	  	$data['page_title'] = "Loans";
+	  	$data['subpage_title'] = "overview & stats";
+		$data['module'] = "manager";
+		$data['view_file'] = "list_loans";
+	
+		$data['loans'] = $this->dataTable_loans('date_of_application');
+		// echo "<pre>";print_r($data['loans']);die();
+		echo Modules::run('template/manager', $data);
+	}
+	function dataTable_loans($orderby)
+	{
+		$loans_data = '';
+		$loans = $this->loans->get($orderby)->result_array();
+		// echo "<pre>";print_r($loans);die();
+		$count = 1;
+		if ($loans) {
+			foreach ($loans as $key => $value) {
+				$loans_data .= '<tr>
+									<td>'.$count.'</td>
+									<td>'.$value['user_id'].'</td>
+									<td>'.$value['loan_amount'].'</td>
+									<td>'.$value['loan_purpose'].'</td>
+									<td>'.$value['guarantor1'].'</td>
+									<td>'.$value['guarantor2'].'</td>
+									<td>'.$value['is_paid'].'</td>
+									<td>'.$value['status'].'</td>
+									<td><a href="'.base_url().'manager/loan_preview/'.$value['loan_id'].'"><button class="btn btn-primary">Preview Loan</button></a></td>
+								</tr>';
+				$count++;
+			}
+		} else {
+			$loans_data = '<tr>
+								<td colspan="8">No Loan Data Found Yet</td>
+							</tr>';
+		}
+		return $loans_data;
+	}
+
+	function loan_preview($loan_id){
+		$this->load->model('m_manager');
+		$loan_details = $this->loans->_custom_query($this->m_manager->get_loan_details($loan_id))->result_array();
+		echo "<pre>";print_r($loan_details);
 	}
 }
 ?>
